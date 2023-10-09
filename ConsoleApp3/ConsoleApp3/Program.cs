979d7f8a6a3f94feaffa5e813ea4ee8311340ca7
XxXiIiOoO <xacha123456@gmail.com>
2023-10-09 23:53:43 +0300

    da
    da

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int currentOctave = 0;
+
+    static int[][] octaves = new int[][]
+    {
+        new int[] { 261, 293, 329, 349, 392, 440, 493 }, 
+        new int[] { 523, 587, 659, 698, 784, 880, 987 }, 
+      
+    };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в консольное пианино!");
+        Console.WriteLine("Используйте клавиши A, S, D, F, G, H, J для игры нот");
+        Console.WriteLine("Используйте клавишу Space для переключения октавы");
+        Console.WriteLine("Нажмите клавишу Esc для выхода");
+
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.Escape)
+                    break;
+
+                if (key == ConsoleKey.Spacebar)
+                {
+                    SwitchOctave();
+                    continue;
+                }
+
+                PlaySound(key);
+            }
+        }
+    }
+
+    static void PlaySound(ConsoleKey key)
+    {
+        int noteIndex = GetNoteIndex(key);
+
+        if (noteIndex != -1)
+        {
+            int frequency = octaves[currentOctave][noteIndex];
+            Console.Beep(frequency, 500); 
+        }
+    }
+
+    static void SwitchOctave()
+    {
+        currentOctave++;
+
+        if (currentOctave >= octaves.Length)
+            currentOctave = 0;
+
+        Console.WriteLine($"Переключена октава {currentOctave + 1}");
+    }
+
+    static int GetNoteIndex(ConsoleKey key)
+    {
+        Dictionary<ConsoleKey, int> keyToNoteIndex = new Dictionary<ConsoleKey, int>()
+        {
+            { ConsoleKey.A, 0 },
+            { ConsoleKey.S, 1 },
+            { ConsoleKey.D, 2 },
+            { ConsoleKey.F, 3 },
+            { ConsoleKey.G, 4 },
+            { ConsoleKey.H, 5 },
+            { ConsoleKey.J, 6 },
+        };
+
+        if (keyToNoteIndex.ContainsKey(key))
+            return keyToNoteIndex[key];
+
+        return -1;
+    }
+}
